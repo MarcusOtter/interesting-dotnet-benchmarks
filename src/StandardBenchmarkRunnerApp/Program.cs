a6425e582b266921d54bca1bb03203b4a110210b
MarcusOtter <github@otterstrom.dev>
2020-09-28 15:59:49 +0200

    Split thorough run with all monikers to separate project
    The project is excluded from the solution until .NET 5 is released and supported in normal Visual Studio. It can be manually added and ran if necessary.

--- src/StandardBenchmarkRunnerApp/Program.cs
+++ src/StandardBenchmarkRunnerApp/Program.cs
@@ -1,15 +1,15 @@
 ﻿using Benchmarks.Running;
 
-namespace ConsoleApplication
+namespace StandardBenchmarkRunnerApp
 {
     internal class Program
     {
         // To run benchmarks:
         //     1. Set the build configuration to "Release"
         //     2. Build the solution (default Visual Studio shortcut is CTRL + SHIFT + B)
-        //     3. Run src/ConsoleApplication/bin/Release/netcoreapp3.1/ConsoleApplication.exe
+        //     3. Run src/StandardBenchmarkRunnerApp/bin/Release/netcoreapp3.1/StandardBenchmarkRunnerApp.exe
         //     4. Follow the instructions in the terminal to select which benchmarks to run
-        
+
         private static void Main(string[] args) 
             => InterestingDotnetBenchmarksRunner.RunBenchmarks(args);
     }
---
+++ src/ThoroughBenchmarkRunnerApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Benchmarks.Running;
+
+namespace ThoroughBenchmarkRunnerApp
+{
+    internal class Program
+    {
+        // For instructions on how to run, see Program.cs in StandardBenchmarkRunnerApp
+        // This application will run the benchmarks on both .NET Core 3.1, .NET Core 2.1 and .NET Framework 4.8.
+        // To only run on .NET Core 3.1, build StandardBenchmarkRunnerApp instead.
+
+        private static void Main(string[] args)
+            => InterestingDotnetBenchmarksRunner.RunBenchmarks(args, runAllMonikers: true);
+    }
+}
